bcc5c72c1cf92b52d1ad65545a370c872bfcac7f
AndreFilipeee <Carpetess>
2024-06-28 00:07:01 +0100

    Did the parsing of the input and stored it in a struct

--- CLI_app/src/main.rs
+++ CLI_app/src/main.rs
@@ -1,3 +1,40 @@
+use std::env;
+use std::error::Error;
+use std::process;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::build(&args).unwrap_or_else(|err|{
+        println!("Problem with parsing arguments: {err}");
+        process::exit(1);
+    });
+    
+    run(config);
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>  {
+ 
+    let contents = fs::read_to_string(config.file_path)?; 
+    
+    println!("{contents}");
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str>{   
+        if args.len() < 3 {
+            return Err("There need to be 2 arguments!");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Ok(Config { query, file_path })
+    }
 }
---
+++ CLI_app/target/debug/CLI_app.d
@@ -0,0 +1,1 @@
+/home/andre/rust/learningrust/CLI_app/CLI_app/target/debug/CLI_app: /home/andre/rust/learningrust/CLI_app/CLI_app/src/main.rs
