c6f8fe85ad7ed3e77dc571cd8e5e127f034d8b16
AndreFilipeee <Carpetess>
2024-06-28 15:40:51 +0100

    Removed tests from this file

--- CLI_app/src/lib.rs
+++ CLI_app/src/lib.rs
@@ -66,33 +66,3 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
 
     results
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn one_result() {
-        let query = "duct";
-        let contents = "/
-Rust:
-safe, fast, productive.
-Pick three.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-    #[test]
-    fn case_incensitive() {
-        let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(vec!["Rust:", "Trust me."], 
-        search_case_insensitive(query, contents)
-        );
-   }
-}
