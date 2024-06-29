dbe4bf659b975d3554fb3a9ea98718656b70bb0c
AndreFilipeee <Carpetess>
2024-06-29 13:00:47 +0100

    Final Version

--- CLI_app/target/debug/deps/CLI_app-503d65b793fd1de9.d
+++
@@ -1,5 +0,0 @@
-/home/andre/rust/learningrust/CLI_app/CLI_app/target/debug/deps/libCLI_app-503d65b793fd1de9.rmeta: src/main.rs
-
-/home/andre/rust/learningrust/CLI_app/CLI_app/target/debug/deps/CLI_app-503d65b793fd1de9.d: src/main.rs
-
-src/main.rs:
